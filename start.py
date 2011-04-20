38208a71d4b4abbab13b64ed6959d12e00dd940e
Tomasz Biczel <el.biczel@gmail.com>
2011-04-20 22:24:03 +0200

    Nice version

--- start.py
+++ start.py
@@ -10,8 +10,10 @@ _bord = 2
 _blurs = 2
 _sizeThreshold = 2
 _sizeFactor = 0.
-_threshold = 1.5
+_threshold = 1.45
 _maxDiff = 1000000
+_joins = 2
+_cutoff = 15
 
 def getNewSize(oldSize):
 	newHeight = 15
@@ -39,6 +41,7 @@ class MyImage:
 		self.img = img.copy()
 		self.name = name
 		self.distances = {name: 0.}
+		self.sizePenalty = {name: 0.}
 	
 	def prepare(self):
 		self.origSize = self.img.size
@@ -52,20 +55,21 @@ class MyImage:
 		self.img = src
 		self.arr = array(map(_RGBToGray, self.img.getdata()))
 	
-	def distance(self, other):
+	def distance(self, other):	
 		if other.name in self.distances:
-			return self.distances[other.name]	
+			return self.distances[other.name] + _sizeFactor * self.sizePenalty[other.name]
 		if (math.fabs(other.origSize[0] - self.origSize[0]) > _sizeThreshold or math.fabs(other.origSize[1] - self.origSize[1]) > _sizeThreshold) or (self.arr.size != other.arr.size):
 			result = _maxDiff
+			size_diff = 0.
 		else:
 			size_diff = math.fabs(other.origSize[0] * other.origSize[1] - self.origSize[0] * self.origSize[1])
-			result = (linalg.norm(self.arr - other.arr) + _sizeFactor * size_diff)
+		 	result = linalg.norm(self.arr - other.arr)
 		self.distances[other.name] = result
 		other.distances[self.name] = result
-		return result
+		self.sizePenalty[other.name] = size_diff
+		other.sizePenalty[self.name] = size_diff
+		return result + _sizeFactor * size_diff
 	
-	
-
 
 	
 def inCluster(imgNo, clusterNo):
@@ -79,7 +83,28 @@ def inCluster(imgNo, clusterNo):
 			return True
 	return False
 
-	
+def join(c, d):
+	cSize = len(clusters[c])
+	dSize = len(clusters[d])
+	sampleSize = _cutoff
+	if (cSize <= _cutoff) or (dSize <= _cutoff):
+		sampleSize = min(cSize, dSize)
+	cReps = random.sample(clusters[c], sampleSize)
+	dReps = random.sample(clusters[d], sampleSize)
+	similar = 0
+	found = 0
+	for cRep in clusters[c]:
+		for dRep in clusters[d]:
+			if images[cRep].distance(images[dRep]) < _threshold:
+				return True
+	return False
+
+def sizeCompare(a, b):
+	return cmp(len(b), len(a))
+
+def sortBySize():
+	clusters.sort(sizeCompare)
+
 def cluster():
 	for x in xrange(len(images)):
 		added = False
@@ -89,6 +114,20 @@ def cluster():
 				clusters[c].append(x)
 		if not added:
 			clusters.append([x])
+
+def joinClusters():
+	global _threshold
+	global _sizeFactor
+	global _addedThreshold
+	_threshold += 0.125
+	_sizeFactor += 0.2
+	for x in xrange(len(clusters)):
+		toJoin = []
+		for y in xrange(x+1, len(clusters)):
+			if join(x, y):
+				toJoin.append(y)
+		for y in reversed(toJoin):
+			clusters[x].extend(clusters.pop(y))
 
 if __name__ == '__main__':
     import sys
@@ -106,6 +145,12 @@ if __name__ == '__main__':
 		for img in images:
 			img.prepare()
 		cluster()
+		print 'clusters: %0s' %  len(clusters)
+		for i in xrange(_joins):
+			sortBySize()
+			joinClusters()
+			print 'clusters: %0s' %  len(clusters)
+		sortBySize()
 		with open(out_file_name + '.txt', 'w') as out:
 			for cluster in clusters:
 				for i in cluster:
